35f3a13fbbf705c8914c6e37f3047ce6ad1707bb
Vesku18 <vesa.simila@yahoo.com>
2021-11-12 23:07:53 +0200

    nelonen kaikki done

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,10 +1,8 @@
+var _ = require('lodash')
+
 const dummy = (blogs) => {
     // ...
     return 1
-  }
-  
-  module.exports = {
-    dummy
   }
 
 const totalLikes = (blogs) => {
@@ -14,7 +12,37 @@ const totalLikes = (blogs) => {
     return uusLista
 }
 
+const favoriteBlog = (blogs) => {
+  const reducer = (a,b) => ( a.likes>b.likes ? a : b)
+  uusLista = blogs.reduce(reducer, 0)
+  console.log(uusLista)
+  return uusLista
+}
+
+const mostBlogs = (blogs) => {
+  const aCount = _.countBy(blogs, (n) => {return n.author})
+  const mostBlogs = _.reduce(_.toPairs(aCount), (a,b) => (a > b ? a : b ))
+  return(mostBlogs[0])
+}
+
+const mostLikes = (blogs) => {
+  const grouped = _.groupBy(blogs, (n) => {return n.author})
+  let most= 0
+  let sums={}
+  for (const [nimi,lista] of _.toPairs(grouped)) {
+    let a = _.sumBy(lista,(o) => {return o.likes })
+    if (a>most) {
+        sums=({'name': nimi, 'likes':a})
+        most = a
+    }
+  }
+  return(sums)
+}
+
 module.exports = { 
     dummy, 
     totalLikes,
+    favoriteBlog,
+    mostBlogs,
+    mostLikes
 }
